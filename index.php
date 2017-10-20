01eabe13f82c1e4a03d3631382465765a9c9c152
Leo Wattier <leo.wattier@free.fr>
2017-10-20 10:30:14 +0200

    Update index.php
    Commandes de test de connexion

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+$host_name = "192.168.99.100";
+$database = "information_schema";
+$user_name = "root";
+$password = "esgi";
+$port = "3306";
 
-	echo "Bienvenue sur notre application PHP";
+$connect = mysqli_connect($host_name, $user_name, $password, $database, $port);
 
+if(mysqli_connect_errno())
+{
+echo '<p>La connexion au serveur MySQL a échoué: '.mysqli_connect_error().'</p>';
+}
+else
+{
+echo '<p>Connexion au serveur MySQL établie avec succès.</p>';
+
+}
 ?>
